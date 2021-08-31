a928fde208145d15449ef93505ad65756ec56bd2
[USER_NOT_FOUND] <35504689+purplehead@users.noreply.github.com>
2021-08-31 19:17:46 +0200

    Fixed NoiseProvider

--- src/at/terranova/generation/CustomChunkGenerator.java
+++ src/at/terranova/generation/CustomChunkGenerator.java
@@ -11,24 +11,23 @@ import java.util.Random;
 public class CustomChunkGenerator extends ChunkGenerator {
 
     private static final int CHUNK_SIZE = 16;
-    private static final int SEA_LEVEL = 63;
+    private static final int SEA_MAX_LEVEL = 63;
+    private static final int SEA_MIN_LEVEL = 25;
 
     @Override
     public void generateNoise(WorldInfo worldInfo, Random random, int chunkX, int chunkZ, ChunkData chunkData) {
-        NoiseProvider provider = new SimplexNoiseProvider(worldInfo.getSeed(), 8);
+        NoiseProvider provider = new SimplexNoiseProvider(worldInfo.getSeed(), 10);
 
         for (int x = 0; x < CHUNK_SIZE; x++) {
             for (int z = 0; z < CHUNK_SIZE; z++) {
                 double height = provider.getHeight(x, z, chunkX, chunkZ, CHUNK_SIZE, 0.9, 1.2);
-                if (height < SEA_LEVEL) {
-                    for (int i = (int) height + 1; i < SEA_LEVEL; i++) {
-                        chunkData.setBlock(x, i, z, Material.WATER);
-                    }
-                    if (height < SEA_LEVEL)
-                        chunkData.setBlock(x, (int) height, z, Material.SAND);
-                } else {
-                    chunkData.setBlock(x, (int) height, z, Material.GRASS_BLOCK);
+
+                // Generate ocean if necessary
+                if (height < SEA_MAX_LEVEL) {
+                    chunkData = generateSea(height, x, z, chunkData);
                 }
+
+                // Generate solid floor
                 int dirtDepth = 2 + random.nextInt(10);
                 for(int i = (int) height - 1; i > height - dirtDepth; i--) {
                     chunkData.setBlock(x, i, z, Material.DIRT);
@@ -36,8 +35,28 @@ public class CustomChunkGenerator extends ChunkGenerator {
                 for (int i = (int) height - dirtDepth; i > 0; i--) {
                     chunkData.setBlock(x, i, z, Material.STONE);
                 }
+                if (height < SEA_MAX_LEVEL + 4) {
+                    for (int i = (int) height; i > (int) height - (3 + random.nextInt(5)); i--) {
+                        chunkData.setBlock(x, i, z, Material.SAND);
+                    }
+                } else {
+                    chunkData.setBlock(x, (int) height, z, Material.GRASS_BLOCK);
+                }
             }
         }
+    }
+
+    private ChunkData generateSea (double height, int x, int z, ChunkData chunkData) {
+        if (height < SEA_MIN_LEVEL) {
+            height++;
+            for (;height < SEA_MIN_LEVEL; height++) {
+                chunkData.setBlock(x, (int) height, z, Material.SAND);
+            }
+        }
+        for (int i = (int) height + 1; i < SEA_MAX_LEVEL; i++) {
+            chunkData.setBlock(x, i, z, Material.WATER);
+        }
+        return chunkData;
     }
 
     @Override
